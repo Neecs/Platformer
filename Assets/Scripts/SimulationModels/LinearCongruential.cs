7d78f286b8d6e2bdf4c21480333d6cc1daca80ee
Nicolas Gil <nikosgil12@gmail.com>
2024-05-13 00:54:17 -0500

    air attack combo added

---
+++ Assets/Scripts/SimulationModels/LinearCongruential.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class LinearCongruential
+{
+    private int a;
+    private int g;
+    private int m;
+    private int c;
+    private long xi;
+
+    public LinearCongruential()
+    {
+        g = 31;
+        a = 48271;
+        m = (int)(Math.Pow(2, g)) - 1;
+        c = 0;
+        xi = DateTime.Now.Ticks % m;
+    }
+
+
+    public float RandomNumber()
+    {
+        xi = ((a * xi) + c) % m;
+        float ri = (float)xi / (m - 1);
+        return ri;
+    }
+}
