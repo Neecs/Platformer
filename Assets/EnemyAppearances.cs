4227ef656b9f8e69b0539cbc44175c6257fbbdb3
Nicolas Gil <nikosgil12@gmail.com>
2024-05-25 23:44:38 -0500

    script to manage gameobjects added

---
+++ Assets/EnemyAppearances.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAppearances : MonoBehaviour
+{
+
+
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
